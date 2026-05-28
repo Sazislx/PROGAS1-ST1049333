ce7193bf5a77bc15c3e8ab1bb3d5186f6a499d05
Sazislx <saziximba7@gmail.com>
2026-05-28 15:48:43 +0200

    Update SoundGreet.cs
    added sound code

--- ST10493337_Part2/SoundGreet.cs
+++ ST10493337_Part2/SoundGreet.cs
@@ -11,7 +11,7 @@ namespace ST10493337_Part2
     {
         //Constructure of sound bot
         public SoundGreet()
-        {
+        {//added sound code
             string paths = AppDomain.CurrentDomain.BaseDirectory.Replace(@"\bin\Debug\", @"\greet.wav");
         
             SoundPlayer greet = new SoundPlayer(paths);
